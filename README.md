24b03af7644cdc5787fda2b0d603f4be135d89fa
D-Pagey <dan.page91@gmail.com>
2018-04-23 21:58:23 +0100

    Refactoring code for custom game over message.

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 - [X] I can play a game of Tic Tac Toe with the computer.
 - [X] My game will reset as soon as it's over so I can play again.
 - [X] I can choose whether I want to play as X or O.
+
+This is the 3rd Advanced Front End project for the FreeCodeCamp curriculum. I 
+wrote this app without using any libraries or frameworks, it is all native HTML,
+ CSS and JavaScript. 
+
+The key thing I learnt on this project was how to write a automated player in 
+the game. Rather than write a game theory optimal opponent, I decided to simply 
+randomise the remaining options. 
+
+You can see this [TicTacToe](https://codepen.io/Pagey/pen/vjLVOy) game on 
+CodePen.
--- scripts.js
+++ scripts.js
@@ -8,38 +8,38 @@ const squares = document.querySelectorAll('.square');
 const elements = document.querySelectorAll('.element');
 const resetBtn = document.querySelector('.reset');
 const result = document.querySelector('.result');
+const resultMsg = document.querySelector('.result-message');
 
 /**
  * Global Variables
  */
-let team;
+let player;
+let computer;
 let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 /**
  * Methods
  */
 function chooseTeam(e) {
-  team = e.target.innerHTML;
+  player = e.target.innerHTML;
+  computer = (player === 'X' ? 'O' : 'X');
   document.querySelector('#choose-area').className = 'hidden';
   document.querySelector('#team-area').className = 'team-area';
-  document.getElementById('player').innerHTML = team;
-  document.getElementById('player').className = team;
-  document.getElementById('computer').innerHTML = (team === 'X' ? 'O' : 'X');
-  document.getElementById('computer').className = (team === 'X' ? 'O' : 'X');
+  document.getElementById('player').innerHTML = player;
+  document.getElementById('player').className = player;
+  document.getElementById('computer').innerHTML = computer;
+  document.getElementById('computer').className = computer;
   addClickEvent();
 }
 
-function switchTeam(team) {
-  return (team === 'X' ? 'O' : 'X');
-}
-
 function playerMove(e) {
-  e.target.childNodes[1].innerHTML = team;
-  board[e.target.dataset.value] = team;
-  isGameOver(board, team);
-  team = switchTeam(team);
+  e.target.childNodes[1].innerHTML = player;
+  board[e.target.dataset.value] = player;
+  isGameOver(player);
   e.target.removeEventListener('click', playerMove);
-  computerMove();
+  if (isGameOver(player) === false) {
+    setTimeout(computerMove, 250);
+  }
 }
 
 function getRandomInt(max) {
@@ -50,14 +50,15 @@ function computerMove() {
   let empty = board.filter(element => typeof element === 'number');
   let index = getRandomInt(empty.length);
   let computerChoice = empty[index];
-  squares[computerChoice].childNodes[1].innerHTML = team;
-  board[computerChoice] = team;
-  isGameOver(board, team);
-  team = switchTeam(team);
+  squares[computerChoice].childNodes[1].innerHTML = computer;
+  board[computerChoice] = computer;
+  isGameOver(computer);
   squares[computerChoice].removeEventListener('click', playerMove);
 }
 
-function isGameOver(board, team) {
+function isGameOver(team) {
+  let empty = board.filter(element => typeof element === 'number');
+
   if (
     // Horizontal
     (board[0] === team && board[1] === team && board[2] === team) ||
@@ -71,13 +72,30 @@ function isGameOver(board, team) {
     (board[0] === team && board[4] === team && board[8] === team) ||
     (board[2] === team && board[4] === team && board[6] === team)
   ) {
-    result.showModal();
-    setTimeout(function() {
-      result.close()
-    }, 2250);
-    removeClickEvent();
-    setTimeout(resetGame, 3750);
-  } 
+    if (team === player) {
+      gameOver('Congratulations, you beat the computer!');
+      return true;
+    } else {
+      gameOver('Wow...you lost to a randomised computer!');
+      return true;
+    }
+  } else if (empty.length == 0) {
+    gameOver('Shockingly, it is a Draw!');
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function gameOver(status) {
+  resultMsg.innerHTML = status;
+  result.showModal();
+  resetBtn.removeEventListener('click', resetGame);
+  setTimeout(function() {
+    result.close()
+  }, 2250);
+  removeClickEvent();
+  setTimeout(resetGame, 3750);
 }
 
 function resetGame() {
@@ -90,7 +108,6 @@ function resetGame() {
   document.querySelector('#team-area').className = 'team-area hidden';
 }
   
-
 /**
   * Event Listeners
  */
